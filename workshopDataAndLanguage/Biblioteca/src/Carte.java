5ad1ef638fffbe435487f9786d1e0dd79faa31b5
Simona Roboiu <sroboiu@endava.com>
2016-07-07 17:45:23 +0300

    Structure of ex 2

---
+++ workshopDataAndLanguage/Biblioteca/src/Carte.java
@@ -0,0 +1,52 @@
+
+public class Carte {
+	String titlu;
+	String autor;
+	String editura;
+	int numarPagini;
+
+	public Carte(String titlu, String autor, String editura, int numarPagini) {
+		super();
+		this.titlu = titlu;
+		this.autor = autor;
+		this.editura = editura;
+		this.numarPagini = numarPagini;
+	}
+
+	public Carte() {
+
+	}
+
+	public String getTitlu() {
+		return titlu;
+	}
+
+	public void setTitlu(String titlu) {
+		this.titlu = titlu;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getEditura() {
+		return editura;
+	}
+
+	public void setEditura(String editura) {
+		this.editura = editura;
+	}
+
+	public int getNumarPagini() {
+		return numarPagini;
+	}
+
+	public void setNumarPagini(int numarPagini) {
+		this.numarPagini = numarPagini;
+	}
+
+}
