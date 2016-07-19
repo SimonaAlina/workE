a4912a38071781db1f93ea1c6fe678c420c5683d
Simona Roboiu <sroboiu@endava.com>
2016-07-19 11:53:44 +0300

    initial structure of io stream homework

---
+++ ioStream/src/ioStream/TestMain.java
@@ -0,0 +1,21 @@
+package ioStream;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+	}
+
+	public static int count(String filename) {
+		int count = 0;
+		return count;
+	}
+	
+	public static void reverseWords(String readFile, String writeFile) {
+		
+	}
+	
+	public static void listFiles(String dirName) {
+		
+	}
+}
