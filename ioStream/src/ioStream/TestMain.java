dffdaa8b4ee76e41eb600e818c7f54c9228c1221
Simona Roboiu <sroboiu@endava.com>
2016-07-19 17:25:46 +0300

    in progress ex 2

--- ioStream/src/ioStream/TestMain.java
+++ ioStream/src/ioStream/TestMain.java
@@ -1,6 +1,9 @@
 package ioStream;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -9,6 +12,7 @@ public class TestMain {
 	public static void main(String[] args) {
 
 		System.out.println(count("file1.txt", 5, 'b'));
+		reverseWords("file1.txt", "output_file.txt");
 	}
 
 	public static int count(String fileName, long pos, char c) {
@@ -20,7 +24,7 @@ public class TestMain {
 			int data;
 			while ((data = randomAccessFile.read()) != -1) {
 				char dataChar = (char) data;
-				if(dataChar == c)
+				if (dataChar == c)
 					count++;
 			}
 		} catch (FileNotFoundException ex) {
@@ -40,10 +44,31 @@ public class TestMain {
 	}
 
 	public static void reverseWords(String readFile, String writeFile) {
+		// int bufferSize = 8 * 1024;
+		BufferedReader bufferedReader = null;
+		RandomAccessFile randomAccessFile = null;
+		try {
+			bufferedReader = new BufferedReader(new FileReader(readFile));
+			randomAccessFile = new RandomAccessFile(writeFile, "rw");
+
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				String[] words = line.split(" ");
+				randomAccessFile.seek(0L);
+				for (int i = words.length - 1; i >= 0; --i) {
+					randomAccessFile.writeBytes(words[i] + " ");
+				}
+				randomAccessFile.writeBytes("\n");
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	public static void listFiles(String dirName) {
-
+		File folder = new File("D:/workspace/workE");
 	}
 }
