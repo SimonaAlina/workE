cf40f35549cf7aca650fc988e0101f6af6c4616f
Simona Roboiu <sroboiu@endava.com>
2016-08-09 10:00:23 +0300

    java 8 ex - training

--- collectionsHomework/src/collections/PenguinFun.java
+++ collectionsHomework/src/collections/PenguinFun.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.function.Predicate;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.Comparator;
 
 public class PenguinFun {
 
-	public static void main(String[] args) {
+	public static <R> void main(String[] args) {
 		/*
 		 * Task 1: list + hashSet
 		 */
@@ -28,6 +34,44 @@ public class PenguinFun {
 		for (Penguin peng : penguinList) {
 			System.out.println(peng.toString());
 		}
+		
+		/*
+		 * java 8
+		 */
+//		int numberOfRaces = PenguinRace.values().length;
+//		Comparator<Penguin> comp1 = (p1,p2) -> Double.compare(p1.getAge(), p2.getAge());
+//		Comparator<Penguin> comp2 = (p1,p2) -> p1.getRace().toString().compareTo(p2.getRace().toString());
+//		
+//		Predicate<Penguin> p1 = p -> p.getAge() > 5;
+//		Predicate<Penguin> p2 = p-> p.getRace().equals(PenguinRace.AFRICAN);
+//		
+//		List<Penguin> penguins = IntStream.rangeClosed(1,  10)
+//				.mapToObj(i -> new Penguin("pingu-"+i, PenguinRace.values()[i% numberOfRaces], i))
+//				.filter(p -> p.getAge() > 5) //p1.or(p2) sau: p -> {return p.getAge() > 5 && p.getRace.equals()}
+//				.collect(Collectors.toList());
+//		.count()
+//		.map(p -> p.getAge()).reduce(0.0, (d1, d2) -> d1 + d2)
+//		
+//		.max(comp1.thenComparing(comp2)) => return Optional
+//		
+//		IntStream.range(0, 10).forEach(System.out::print);
+//		
+//		Collectors.joining(",");
+//		.collect(Collectors.groupingBy(p -> p.getRace().toString()));
+//		for (Penguin peng : penguins) {
+//			System.out.println(peng.toString());
+//		}
+//		
+//		List<Integer> l1 = Arrays.asList(1,2,3);
+//		List<Integer> l2 = Arrays.asList(4,5,7);
+//		List<List<Integer>> l3 = Arrays.asList(l1,l2);
+//		
+//		Function<List<Integer>, Stream> function = l -> l.stream();
+//		l3.stream().flatMap((Function<? super List<Integer>, ? extends Stream<? extends R>>) function).forEach(System.out::print);
+//		
+		/*
+		 * end java 8
+		 */
 		System.out.println("\nHashSet: -------------------------");
 		Set<Penguin> penguinHashSet = new HashSet<>();
 		for (int i = 0; i < 20; ++i) {
